9c04b2b53d3c86ffa2e69c44fcc90ec191f8fbc0
Adam Schubert <adam.schubert@sg1-game.net>
2018-06-18 00:20:16 +0200

    Use tempfile.gettempdir()

--- tests/test_backend_filesystem.py
+++ tests/test_backend_filesystem.py
@@ -1,13 +1,14 @@
 """Test backend."""
 
 import os
+import tempfile
 import pytest
 from flask_celery.backends.filesystem import LockBackendFilesystem
 
 
 def test_filesystem_path_exists():
     """Test Not failing when path exists."""
-    path = '/tmp/path_exists_dir'
+    path = os.path.join(tempfile.gettempdir(), 'path_exists_dir')
     if not os.path.isdir(path):
         os.mkdir(path)
     LockBackendFilesystem('file://{}'.format(path))
@@ -15,7 +16,7 @@ def test_filesystem_path_exists():
 
 def test_filesystem_path_exists_file():
     """Test Failing when path exists and it is a file."""
-    path = '/tmp/path_exists_file'
+    path = os.path.join(tempfile.gettempdir(), 'path_exists_file')
     with open(path, 'w') as f:
         f.write('exists')
     with pytest.raises(OSError):
@@ -24,7 +25,7 @@ def test_filesystem_path_exists_file():
 
 def test_filesystem_acquire_exists_empty_file():
     """Test Creation of correct lock file when empty one exists."""
-    path = '/tmp/path_exists_dir'
+    path = os.path.join(tempfile.gettempdir(), 'path_exists_dir')
     lb = LockBackendFilesystem('file://{}'.format(path))
     with open(lb.get_lock_path('identifier'), 'w') as f:
         f.write('')
@@ -34,14 +35,14 @@ def test_filesystem_acquire_exists_empty_file():
 
 def test_filesystem_release_file_not_found():
     """Test release of non existing lock."""
-    path = '/tmp/path_exists_dir'
+    path = os.path.join(tempfile.gettempdir(), 'path_exists_dir')
     lb = LockBackendFilesystem('file://{}'.format(path))
     lb.release('not_found_file')
 
 
 def test_filesystem_release_file_is_not_a_file():
     """Test release of non-file file."""
-    path = '/tmp/path_exists_dir'
+    path = os.path.join(tempfile.gettempdir(), 'path_exists_dir')
     lb = LockBackendFilesystem('file://{}'.format(path))
 
     dir_path = lb.get_lock_path('not_a_file')
@@ -55,7 +56,7 @@ def test_filesystem_release_file_is_not_a_file():
 
 def test_filesystem_exists_exists_empty_file():
     """Test Creation of correct lock file when empty one exists."""
-    path = '/tmp/path_exists_dir'
+    path = os.path.join(tempfile.gettempdir(), 'path_exists_dir')
     lb = LockBackendFilesystem('file://{}'.format(path))
     with open(lb.get_lock_path('identifier'), 'w') as f:
         f.write('')
